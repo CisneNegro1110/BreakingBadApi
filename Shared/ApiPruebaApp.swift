124d3c581e06d04f14d8113afc374db1ce0c8b88
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-06-04 18:40:35 -0600

    Initial Commit

---
+++ Shared/ApiPruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiPruebaApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ApiPruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
